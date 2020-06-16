1f80444772c601d33ad9989136ff27ed72b1862a
root <trungchanh770@yahoo.com>
2020-06-16 20:59:13 +0000

    fix async var

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 var passport = require("passport")
 var User = require("../models/user")
-var async1 = require("async");
+var async = require('async');
 var nodemailer = require("nodemailer");
 var crypto = require('crypto')
 //root route
